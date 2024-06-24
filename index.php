c862e759db2a8fd93a1cf58159d54aa9c73dc820
Chanuka Dilshan <123976851+Chanukaa2002@users.noreply.github.com>
2024-06-24 22:27:12 +0530

    add new featurs

--- index.php
+++ index.php
@@ -4,43 +4,137 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <!-- Link Style.css -->
-    <link rel="stylesheet" href="style.css" />
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
     <!-- Bootstrap Bundle JS (includes Popper.js) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+    <style>
+        button {
+        border-radius: 13px;
+        }
+        h1 {
+        font-family: "palatino linotype", palatino, serif;
+        color: azure;
+      }
+      label{
+        color: azure;
+      }
+        body {
+            margin: 0;
+            padding: 0;
+            background: linear-gradient(100deg, #000915, #003465);
+        }
+        .top_pannel{
+            display: flex;
+            flex-direction: row;
+        }
+        .btns{
+            margin: 10px;
+            padding: 5px;           
+            margin-top: -50px;
+            font-family: "palatino linotype";
+            border:none;
+            margin-left: -40px;
+        }
+        .btn-login{
+            margin-right: 20px;
+        }
+        .logo{
+            width: 75px;
+            height: 75px;
+        }
+        .card{
+            height: 250px;
+            width: 250px;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body class="body">
-    <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-        <a class="navbar-brand text-white" href="#">MovieTickets.lk</a>
-        <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon "></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Movies</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="signIn.php">Sign In</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="signUp.php">Sign Up</a>
-                </li>
-            </ul>
+    <!--|nav Bar|-->
+    <div class="column">
+        <div class="row">
+            <a href="#"><img src="src/Logo.png" class="logo"></a>
         </div>
-    </nav>
-
-    <?php
-    session_start();
-    if (isset($_SESSION["UserName"])) {
-        // Modify the navigation bar to show user dp instead of Sign In and Sign Up links
-    }
-    ?>
+        <div class="row">
+            <div class="d-flex justify-content-end btns">
+                <a href="signIn.php"><button class="btn-login">LogIn</button></a>
+                <a href="signUp.php"><button >SignUp</button></a>             
+            </div>
+        </div>
+    </div>
+        <!--|Movie List|-->
+    <div class="container">
+        <h1>Movies</h1>
+        <div class="row mt-5">
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="src/img1.png" alt="Film 1">
+                    <div class="card-body">
+                        <h4 class="card-title">Film-1</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- signIn.php
+++ signIn.php
@@ -4,14 +4,69 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!--Link Style.css-->
+    <!--Link Style.css
     <link rel="stylesheet" href="style.css">
-    
+    -->
     <!--Boostrap CDN-->
-    <link rel="stylesheet" href=" 	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src=" 	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" > </script>
 
-
+    <style>
+      button {
+        border-radius: 30px;
+      }
+      h1 {
+        font-family: "palatino linotype", palatino, serif;
+        color: azure;
+      }
+      label{
+        color: azure;
+      }
+      body {
+        margin: 0;
+        padding: 0;
+        background: linear-gradient(100deg, #000915, #003465);
+      }
+      .userImg {
+          width: 30px;
+          height: 30px;
+          margin-top: 10px;
+        }
+      .pwImg {
+          width: 30px;
+          height: 30px;
+          margin-top: 10px;
+        }
+        .userName {
+          display: flex;
+          flex-direction: row;
+        }
+        .password {
+          display: flex;
+          flex-direction: row;
+        }
+        .forget-pass {
+          text-decoration: none;
+          color: #5b548a;
+        }
+        .form-control {
+          margin-left: 10px;
+        }
+        .forget-pass:hover {
+          color: #1e7dea;
+        }
+        .create {
+          text-decoration: none;
+          color: #5b548a;
+        }
+        .create:hover {
+          color: #1e7dea;
+        }
+        .btn-signIn{
+          background-color: #06d001 !important;
+          border-color: #06d001;
+        }
+    </style>
     <title>Sign In</title>
 </head>
 <body class="body">
@@ -20,26 +75,26 @@
     <div class="row d-flex align-items-center justify-content-center h-100">
       <div class="col-md-8 col-lg-7 col-xl-6">
         <img id="genarateImg"
-          class="img-fluid " src="src/login2.png" alt="Phone image">
+          class="img-fluid " src="src/img1.png" alt="Phone image">
       </div>
       <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
         <form>
           <h1 class="text-center">Sign In</h1>
           <!-- userName input -->
           <div data-mdb-input-init class="form-outline mb-4 userName">
-            <img src="src/user.png" alt="user" class="userImg">
+            <img src="src/User.png" alt="user" class="userImg">
             <input type="text" name="userName" class="form-control form-control-lg" placeholder="UserName"/>
           </div>
 
           <!-- Password input -->
           <div data-mdb-input-init class="form-outline mb-4 password">
-          <img src="src/padlock.png" alt="pw" class="pwImg">
+          <img src="src/Padlock.png" alt="pw" class="pwImg">
             <input type="password" name="password" class="form-control form-control-lg" placeholder="Password"/>
           </div>
 
           <div class="d-flex justify-content-around align-items-center mb-4">
             <!-- Checkbox -->
-            <div class="form-check">
+            <div>
               <input class="form-check-input" type="checkbox" value="" name="rememberMe" checked />
               <label class="form-check-label" for="form1Example3"> Remember me </label>
             </div>
@@ -50,9 +105,8 @@
                 </div>
             </div>
           </div>
-
           <!-- Submit button -->
-          <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg btn-block btn-signIn-signUp" name="btnSignIn">Sign in</button>
+          <button type="submit"class="btn btn-primary btn-lg btn-block btn-signIn" name="btnSignIn">Sign in</button>
         </form>
       </div>
     </div>
@@ -60,8 +114,6 @@
 </section>
 </body>
 </html>
-
-
 
 <?php
   if(isset($_POST["btnSignIn"])){
@@ -87,8 +139,10 @@
           if(mysqli_fetch_array($result)>0){//
 
             if(isset($_POST["rememberMe"])){ //* set cookies-------------------------------------------------
-              setcookie("UserName","$userName",time()+3600,"/","",0);
-              setcookie("Password","$password",time()+3600,"/","",0);
+              //one mounth cookie
+              $expire = 30*24*3600;
+              setcookie("UserName","$userName",time()+$expire,"/","",0);
+              setcookie("Password","$password",time()+$expire,"/","",0);
 
               //* start session-------------------------------------------------
               session_start();
@@ -97,8 +151,7 @@
           }
           else{
             echo "UserName or Password wrong!";
-          }
-          
+          }         
         }
   }
 ?>
