d2e6f5375f6a84696babf76ebbe15985bbe4fd6e
Chanuka Dilshan <123976851+Chanukaa2002@users.noreply.github.com>
2024-06-25 09:34:30 +0530

    add new

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
@@ -10,44 +11,11 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <style>
-        button {
-        border-radius: 13px;
-        }
-        h1 {
-        font-family: "palatino linotype", palatino, serif;
-        color: azure;
-      }
-      label{
-        color: azure;
-      }
-        body {
-            margin: 0;
-            padding: 0;
-            background: linear-gradient(100deg, #000915, #003465);
-        }
-        .top_pannel{
-            display: flex;
-            flex-direction: row;
-        }
-        .btns{
-            margin: 10px;
-            padding: 5px;           
-            margin-top: -50px;
-            font-family: "palatino linotype";
-            border:none;
-            margin-left: -40px;
-        }
-        .btn-login{
-            margin-right: 20px;
-        }
-        .logo{
-            width: 75px;
-            height: 75px;
-        }
-        .card{
-            height: 250px;
-            width: 250px;
-        }
+        
+        
+
+/*----  Main Style  ----*/
+    
     </style>
     <title>Document</title>
 </head>
@@ -58,13 +26,13 @@
             <a href="#"><img src="src/Logo.png" class="logo"></a>
         </div>
         <div class="row">
-            <div class="d-flex justify-content-end btns">
-                <a href="signIn.php"><button class="btn-login">LogIn</button></a>
-                <a href="signUp.php"><button >SignUp</button></a>             
+            <div class="d-flex justify-content-end" id="btns">
+                <a href="signIn.php"><button class="btn-login" id="btn">LogIn</button></a>
+                <a href="signUp.php" ><button id="btn">SignUp</button></a>             
             </div>
         </div>
     </div>
-        <!--|Movie List|-->
+        <!--|Movie List|
     <div class="container">
         <h1>Movies</h1>
         <div class="row mt-5">
@@ -136,5 +104,133 @@
             </div>
         </div>
     </div>
+    -->
+       <!-- Topic Cards -->
+    <div class="cards_landscape_wrap-2">
+        <div class="container">
+            <h1 class="movie-heading">Movies</h1>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie1.jpg" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>Deadpool&Wolverine</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie2.jpg" alt="" />
+                                </div>
+                                <div class="text-container">                                    
+                                    <h6>Fall Guy</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie3.jpg" alt="" />
+                                </div>
+
+                                <div class="text-container">
+                                    <h6>IF</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie4.jpg" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>The Strangers</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie5.jpg" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>Longlegs 7.12</h6>
+                                    </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie6.jpg" alt="" />
+                                </div>
+                                <div class="text-container">                                    
+                                    <h6>Inside Out 2</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie7.jpg" alt="" />
+                                </div>
+
+                                <div class="text-container">
+                                    <h6>Bad Boys</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="src/movie8.jpg" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>Planet of the Apes</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 </body>
 </html>
